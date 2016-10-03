750f3ae530d0c179b6ce7c2d2770cb9c01bbaf3a
dawei <weiweicat333@gmail.com>
2016-10-04 01:15:42 +0800

    updata

--- js/lib/dom/selectTemplate.js
+++ js/lib/dom/selectTemplate.js
@@ -1,7 +1,19 @@
+/**
+* @fileoverview 用于创建html元素，并插入相应的DOM文档
+* @author dawei
+* @history 2016/10/3
+*/
+
 define(function(require, exports, module) {
 
 	var $ = require('jquery');
 
+	/**
+	* 创建同层元素
+	* @param {String} method jquery方法,用来选择jquery的dom操作方法
+	* @param {String} css jquery选择器,用来获取插入的元素
+	* @param {Object} opt 选择创建元素，和元素的属性(其中属性用数组来表示)
+	*/
 	exports.creatElement = function (method, css, opt) {
 		var str = '';
 		for(var key in opt) {
@@ -20,6 +32,12 @@ define(function(require, exports, module) {
 		$(css)[method](str);
 	};
 
+	/**
+	* 创建嵌套元素
+	* @param {String} method jquery方法,用来选择jquery的dom操作方法
+	* @param {String} css jquery选择器,用来获取插入的元素
+	* @param {Object} opt 选择创建元素，和元素的属性(其中属性用数组来表示)
+	*/
 	exports.createInsertElement = function(method, css, opt) {
 		var str = '';
 		var strEnd = '';
--- js/pl/init.js
+++ js/pl/init.js
@@ -1,6 +1,19 @@
 define(function(require, exports, module) {
 
-	var a = require('./indexLogin/select');
-	a.run();
+	var $ = require('jquery');
+	var selecTemp = require('../lib/dom/selectTemplate');
 
+	exports.run = function () {
+		selecTemp.createInsertElement('append', '.login', {
+			div : [{class : 'btn-group-vertical selectLogin', id : 'login'}],
+			button : [{class : 'btn btn-primary', nodeValue : '企业登录'}, 
+					{class : 'btn btn-primary', nodeValue : '个人登录'}]
+		});
+		$('#login').on('click', function(evt) {
+			switch($(evt.target).text()) {
+				case '企业登录' : window.open('company/index_start.html', '_self');break;
+				case '个人登录' : window.open('personal/index_start.html', '_self');break;
+			}
+		});
+	};
 });
